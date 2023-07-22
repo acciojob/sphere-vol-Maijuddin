0512ff3497b7d9e3df52cf8bae0bbcbd1702f5b9
Maijuddin Mallick <124431155+Maijuddin@users.noreply.github.com>
2023-07-22 23:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radius = (document.getElementById("radius").value);
+
+  if (isNaN(radius) || radius < 0) {
+    alert("Please enter a valid positive number for the radius.");
+    return;
+  }
+
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  document.getElementById("volume").value = volume;
+	// const radius = document.getElementById("radius").value;
+	
+	// let volumeOfSphere = (4/3) * Math.PI * Math.pow(radius, 3);
+
+	// document.getElementById("volume").value = volumeOfSphere
+	// 	// .toFixed(2);
+   
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
